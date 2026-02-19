8d34bb86c5c25fafcaa821a7ff2fcbeb35082b5e
Vinícius <vinicius.bornhofen47@gmail.com>
2026-02-18 21:44:45 -0300

    fim aula1

---
+++ src/UNI1/aula1/main.java
@@ -0,0 +1,21 @@
+package UNI1.aula1;
+
+public class main {
+    public static void main(String[] args) {
+/* 
+        //cria um novo endereco na memoria
+        Aluno a1 = new Aluno(123, "Ana");
+
+        //aponta para o endereco de a1
+        Aluno a2 = a1;
+        a2.setMatricula(7001);
+
+        System.out.println(a2.getNome() + a2.getMatricula());
+        System.out.println(a1.getNome() + a1.getMatricula()); */
+
+        int[] numeros = new int[300000];
+        numeros[157831] = 900;
+        boolean achou = false;
+       // p(157831) = 100 + (157831 * 4);
+    }
+}
